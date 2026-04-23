3f1770742f86e1f23bbb8f1a5b023edf0e5383e6
Xaadim <amouhamadoubambadiouf@gmail.com>
2026-04-23 13:34:55 +0000

    Add files via upload

---
+++ Portfolio.py
@@ -0,0 +1,44 @@
+import streamlit as st
+st.title("PROFIL")
+st.write("""
+Technicien superieur en géomatique et en Géographe spécilaisé en hydrologie à l’Université Cheikh Anta Diop de Dakar, je développe des compétences à l’intersection de la cartographie, de l’analyse spatiale et des sciences de l’eau.
+Par ailleurs,je travaille sur l’exploitation des données géographiques pour analyser les phénomènes environnementaux, notamment les risques hydrologiques et la gestion des ressources en eau.
+À travers mes projets, j’utilise des outils SIG comme QGIS pour produire des analyses et des cartes utiles à la prise de décision.
+Je suis actuellement à la recherche d’opportunités pour contribuer à des projets concrets et approfondir mon expertise.
+""")
+st.header("PARCOURS ACADÉMIQUE")
+st.markdown("""
+* MASTER EN HYDROLOGIE A L'Universite Cheikh Anta Diop de Dakar(UCAD)
+* LICENCE EN GEOGRAPHIE
+* BREVET DE TECHNICIEN SUPERIEUR EN GÉOMATIQUE au Centre D'Entrepreneuriat et de Developpement Technique(CEDT LE G15)
+* BACCALAURÉAT
+""")
+st.subheader("CERTIFICATONS")
+st.markdown("""
+* Certificat en Informatique et Internet
+* Design et Audiovisuel
+""")
+st.header("COMPÉTENCES")
+st.markdown("""
+* Géoreferencement
+* Modélisation
+* Numérisation
+* Cartographie 
+* Suite office
+* Python
+* OTC(Outils de Travail Collaboratif)
+* AUTOCAD
+* Analyse spatiale
+* Collecte de données avec les outils comme GPS,KBOCOLLECTS
+""")
+with st.sidebar:
+    
+    st.title("Mouhamadou Bamba Diouf")
+    
+    st.header("Technicien superieur en Géomatique , Géographe")
+    st.header("Informations personnelles")
+    st.write("""
+    Adresse:Dakar,Sénégal
+    Email:amouhamadoubambadiouf@gmail.com
+    """)
+    
